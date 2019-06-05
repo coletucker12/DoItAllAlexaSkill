02d19299e51b0dfd588a3d5163fd6fa600e5c404
ogcolet <ctucker@firstorion.com>
2019-06-04 22:43:13 -0500

    Swapping to decorator handlers instead of class defintions and a working weather handler

--- src/lifx.py
+++ src/lifx.py
@@ -2,176 +2,116 @@ import requests
 import os
 from dotenv import load_dotenv
 import logging
-
 from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
-
 from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 
 sb = SkillBuilder()
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 load_dotenv()
 
 
-class LaunchRequestHandler(AbstractRequestHandler):
+@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
+def launch_request_handler(handler_input):
     """ Handler for the LIFX Lights Skill"""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_request_type("LaunchRequest")(handler_input)
+    # type: HandlerInput -> Response
+    speech_text = "Welcome to the LIFX Skills Kit, what would you like to do?"
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "Welcome to the LIFX Skills Kit, what would you like to do?"
-
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello!", speech_text)).set_should_end_session(
+    handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello!", speech_text)).set_should_end_session(
             False)
-        return handler_input.response_builder.response
-
-class GetWeatherIntentHandler(AbstractRequestHandler):
-    """ Handler for Turning On ALL lights"""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("GetCurrentWeather")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        # speech_text = "Here is the current forecast!"
-
-        # LATITUDE = "34.746483"
-        # LONGITUDE = "-92.289597"
-        weather_request = "https://api.darksky.net/forecast/75a9e562db1e04cbb51a8af5ee22b6b5/34.746483,-92.289597?exclude=flags, minutely"
-        weather_json = requests.get(weather_request).json()
-        speech_text = weather_json['timezone']
-
-        handler_input.response_builder.speak(speech_text).set_card(SimpleCard("Hello!", speech_text))
-        return handler_input.response_builder.response
+    return handler_input.response_builder.response
 
 
-class TurnOnAllLightsIntentHandler(AbstractRequestHandler):
-    """ Handler for Turning On ALL lights"""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("TurnOnAllLights")(handler_input)
+@sb.request_handler(can_handle_func=is_intent_name("GetCurrentWeather"))
+def get_current_weather_handler(handler_input):
+    # type: (HandlerInput) -> Response
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "Turning On All Of The Lights"
+    # speech_text = "Here is the current forecast!"
+    # LATITUDE = "34.746483"
+    # LONGITUDE = "-92.289597"
+    weather_request = "https://api.darksky.net/forecast/75a9e562db1e04cbb51a8af5ee22b6b5/34.746483,-92.289597?exclude=flags, minutely"
+    weather_json = requests.get(weather_request).json()
+    speech_text = weather_json['timezone']
 
-        reprompt = "Anything else?"
-        handler_input.response_builder.speak(speech_text).ask(reprompt)
-        return handler_input.response_builder.response
-
-class HelloWorldIntentHandler(AbstractRequestHandler):
-    """Handler for Hello World Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("HelloWorldIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "Hello Python World from Classes!"
-
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(
-            True)
-        return handler_input.response_builder.response
+    handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello!", speech_text)).ask("Anything else?")
+    return handler_input.response_builder.response
 
 
-class HelpIntentHandler(AbstractRequestHandler):
-    """Handler for Help Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+@sb.request_handler(can_handle_func=is_intent_name("TurnOnAllLights"))
+def turn_on_all_lights_handler(handler_input):
+    # type: (HandlerInput) -> Response
+    speech_text = "Turning On All Of The Lights"
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "You can say hello to me!"
+    weather_request = "https://api.darksky.net/forecast/75a9e562db1e04cbb51a8af5ee22b6b5/34.746483,-92.28959"
+    weather_json = requests.get(weather_request).json()
+    speech_text += weather_json['timezone']
 
-        handler_input.response_builder.speak(speech_text).ask(
-            speech_text).set_card(SimpleCard(
-                "Hello World", speech_text))
-        return handler_input.response_builder.response
+    reprompt = "Anything else?"
+    handler_input.response_builder.speak(speech_text).set_should_end_session(False).ask(reprompt)
+    return handler_input.response_builder.response
 
 
-class CancelOrStopIntentHandler(AbstractRequestHandler):
-    """Single handler for Cancel and Stop Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                is_intent_name("AMAZON.StopIntent")(handler_input))
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
+def help_intent_handler(handler_input):
+    # type: (HandlerInput) -> Response
+    speech_text = "You can say hello to me!"
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "Goodbye!"
-
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text))
-        return handler_input.response_builder.response
+    handler_input.response_builder.speak(speech_text).ask(speech_text).set_card(
+        SimpleCard("Hello World", speech_text))
+    return handler_input.response_builder.response
 
 
-class FallbackIntentHandler(AbstractRequestHandler):
-    """AMAZON.FallbackIntent is only available in en-US locale.
+@sb.request_handler(
+    can_handle_func=lambda handler_input :
+        is_intent_name("AMAZON.CancelIntent")(handler_input) or
+        is_intent_name("AMAZON.StopIntent")(handler_input))
+def cancel_and_stop_intent_handler(handler_input):
+    # type: (HandlerInput) -> Response
+    speech_text = "Goodbye!"
+
+    handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello World", speech_text))
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.FallBackIntent"))
+def fallback_intent_handler(handler_input):
+    """
+    AMAZON.FallbackIntent is only available in en-US locale.
     This handler will not be triggered except in that locale,
     so it is safe to deploy on any locale.
     """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = (
-            "The Hello World skill can't help you with that.  "
-            "You can say hello!!")
-        reprompt = "You can say hello!!"
-        handler_input.response_builder.speak(speech_text).ask(reprompt)
-        return handler_input.response_builder.response
+    # type: (HandlerInput) -> Response
+    speech_text = (
+        "The Hello World skill can't help you with that.  "
+        "You can say hello!!")
+    reprompt = "You can say hello!!"
+    handler_input.response_builder.speak(speech_text).ask(reprompt)
+    return handler_input.response_builder.response
 
 
-class SessionEndedRequestHandler(AbstractRequestHandler):
-    """Handler for Session End."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_request_type("SessionEndedRequest")(handler_input)
+@sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
+def session_ended_request_handler(handler_input):
+    # type: (HandlerInput) -> Response
+    # any cleanup logic goes here
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        return handler_input.response_builder.response
+    return handler_input.response_builder.response
 
 
-class CatchAllExceptionHandler(AbstractExceptionHandler):
-    """Catch all exception handler, log exception and
-    respond with custom message.
-    """
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
+@sb.exception_handler(can_handle_func=lambda i, e: True)
+def all_exception_handler(handler_input, exception):
+    # type: (HandlerInput, Exception) -> Response
+    # Log the exception in CloudWatch Logs
+    print(exception)
 
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        logger.error(exception, exc_info=True)
+    speech = "Sorry, I didn't get it. Can you please say it again!!"
+    handler_input.response_builder.speak(speech).ask(speech)
+    return handler_input.response_builder.response
 
-        speech = "Sorry, there was some problem. Please try again!!"
-        handler_input.response_builder.speak(speech).ask(speech)
-
-        return handler_input.response_builder.response
-
-
-sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(HelloWorldIntentHandler())
-sb.add_request_handler(HelpIntentHandler())
-sb.add_request_handler(CancelOrStopIntentHandler())
-sb.add_request_handler(FallbackIntentHandler())
-sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(TurnOnAllLightsIntentHandler())
-
-sb.add_exception_handler(CatchAllExceptionHandler())
 
 handler = sb.lambda_handler()
