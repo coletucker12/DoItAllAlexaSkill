6aedd0a47340862eb28668e14fc5f152a60b70aa
ogcolet <ctucker@firstorion.com>
2019-06-06 14:18:09 -0500

    Setup getting weather based on users location set in the alexa app

--- src/lifx.py
+++ src/lifx.py
@@ -5,11 +5,32 @@ import traceback
 
 from dotenv import load_dotenv, find_dotenv
 import logging
-from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.skill_builder import SkillBuilder, CustomSkillBuilder
+from ask_sdk_core.api_client import DefaultApiClient
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
-from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model.ui import SimpleCard, AskForPermissionsConsentCard
 from ask_sdk_model import Response
+from ask_sdk_model.services import ServiceException
+
+
+permissions = ["read::alexa:device:all:address:country_and_postal_code"]
+WELCOME = ("Welcome to the Do It All Smart Home Skill "
+           "You can ask this app to do many things. "
+           "What do you want to ask?")
+WHAT_DO_YOU_WANT = "What do you want to ask?"
+NOTIFY_MISSING_PERMISSIONS = ("Please enable Location permissions in "
+                              "the Amazon Alexa app.")
+NO_ADDRESS = ("It looks like you don't have an address set. "
+              "You can set your address from the companion app.")
+ADDRESS_AVAILABLE = "Here is your full address: {}, {}, {}"
+ERROR = "Uh Oh. Looks like something went wrong."
+LOCATION_FAILURE = ("There was an error with the Device Address API. "
+                    "Please try again.")
+GOODBYE = "Bye! Thanks for using the Do It All Smart Home Skill!"
+UNHANDLED = "This skill doesn't support that. Please ask something else"
+HELP = ("You can use this skill by asking something like: "
+        "Whats the current weather? or Turn on all my lights")
 
 
 def respond(err, res=None):
@@ -30,7 +51,7 @@ except Exception as e:
     print(respond(err))
 
 
-sb = SkillBuilder()
+sb = CustomSkillBuilder(api_client=DefaultApiClient())
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -44,38 +65,86 @@ LITTLE_ROCK_LONG = -92.289597
 def launch_request_handler(handler_input):
     """ Handler for the LIFX Lights Skill"""
     # type: HandlerInput -> Response
-    speech_text = "Welcome to the LIFX Skills Kit, what would you like to do?"
 
-    handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello!", speech_text)).set_should_end_session(
+    handler_input.response_builder.speak(WELCOME).set_card(
+        SimpleCard(WHAT_DO_YOU_WANT)).set_should_end_session(
             False)
     return handler_input.response_builder.response
+
+
+"""
+    ---------------------------------
+    Handlers for Weather
+    ---------------------------------
+"""
 
 
 @sb.request_handler(can_handle_func=is_intent_name("GetCurrentWeather"))
 def get_current_weather_handler(handler_input):
     # type: (HandlerInput) -> Response
 
+    req_envelope = handler_input.request_envelope
+    service_client_fact = handler_input.service_client_factory
+
+    if not (req_envelope.context.system.user.permissions and
+            req_envelope.context.system.user.permissions.consent_token):
+        handler_input.response_builder.speak("You are missing permissions")
+        handler_input.response_builder.set_card(
+            AskForPermissionsConsentCard(permissions=permissions))
+        return handler_input.response_builder.response
+
+    try:
+        device_id = req_envelope.context.system.device.device_id
+        device_addr_client = service_client_fact.get_device_address_service()
+        addr = device_addr_client.get_country_and_postal_code(device_id)
+
+        if addr.postal_code is None and addr.country_code is None:
+            handler_input.response_builder.speak("You have no address")
+        else:
+            weatherObj = DarkSkyApi(addr.postal_code, "current")
+            speech_text = weatherObj.get_weather()
+            handler_input.response_builder.speak(speech_text)
+        return handler_input.response_builder.response
+    except ServiceException:
+        handler_input.response_builder.speak("Looks like something went wrong")
+        return handler_input.response_builder.response
+    except Exception as e:
+        raise e
+
+
+    # handler_input.response_builder.speak(speech_text).set_card(
+    #     SimpleCard("Hello!", speech_text)).ask("Anything else?")
+    # return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("GetHourlyWeather"))
+def get_hourly_weather_handler(handler_input):
+    # type: (HandlerInput) -> Response
+
     weather_coords = {
         "latitude": LITTLE_ROCK_LAT,
         "longitude": LITTLE_ROCK_LONG,
     }
-    print(weather_coords)
 
-    weatherObj = DarkSkyApi("75a9e562db1e04cbb51a8af5ee22b6b5", weather_coords)
-    weather_dict = weatherObj.get_weather()
+    weatherObj = DarkSkyApi(weather_coords, "hourly")
+    speech_text = weatherObj.get_weather()
 
-    if weather_dict:
-        speech_text = "The current forecast is "
-        speech_text += weather_dict["current_weather"]
+    handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello!", speech_text)).ask("Anything else?")
+    return handler_input.response_builder.response
 
-        speech_text += ". The hourly forecast is "
-        speech_text += weather_dict["hourly_weather"]
 
-        speech_text += ". The daily forecast is "
-        speech_text += weather_dict["daily_weather"]
-    else:
-        speech_text = "Sorry, incorrect location for retrieving weather."
+@sb.request_handler(can_handle_func=is_intent_name("GetDailyWeather"))
+def get_daily_weather_handler(handler_input):
+    # type: (HandlerInput) -> Response
+
+    weather_coords = {
+        "latitude": LITTLE_ROCK_LAT,
+        "longitude": LITTLE_ROCK_LONG,
+    }
+
+    weatherObj = DarkSkyApi(weather_coords, "daily")
+    speech_text = weatherObj.get_weather()
 
     handler_input.response_builder.speak(speech_text).set_card(
         SimpleCard("Hello!", speech_text)).ask("Anything else?")
@@ -99,10 +168,8 @@ def turn_on_all_lights_handler(handler_input):
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
 def help_intent_handler(handler_input):
     # type: (HandlerInput) -> Response
-    speech_text = "You can say hello to me!"
 
-    handler_input.response_builder.speak(speech_text).ask(speech_text).set_card(
-        SimpleCard("Hello World", speech_text))
+    handler_input.response_builder.speak(HELP).ask(HELP)
     return handler_input.response_builder.response
 
 
@@ -114,8 +181,7 @@ def cancel_and_stop_intent_handler(handler_input):
     # type: (HandlerInput) -> Response
     speech_text = "Goodbye!"
 
-    handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text))
+    handler_input.response_builder.speak(GOODBYE)
     return handler_input.response_builder.response
 
 
@@ -127,11 +193,21 @@ def fallback_intent_handler(handler_input):
     so it is safe to deploy on any locale.
     """
     # type: (HandlerInput) -> Response
-    speech_text = (
-        "The Hello World skill can't help you with that.  "
-        "You can say hello!!")
-    reprompt = "You can say hello!!"
-    handler_input.response_builder.speak(speech_text).ask(reprompt)
+
+    handler_input.response_builder.speak(UNHANDLED).ask(HELP)
+    return handler_input.response_builder.response
+
+
+@sb.exception_handler(can_handle_func=ServiceException)
+def get_address_exception_handler(handler_input, exception):
+    if exception.status_code == 403:
+        handler_input.response_builder.speak(
+            NOTIFY_MISSING_PERMISSIONS).set_card(
+            AskForPermissionsConsentCard(permissions=permissions))
+    else:
+        handler_input.response_builder.speak(
+            LOCATION_FAILURE).ask(LOCATION_FAILURE)
+
     return handler_input.response_builder.response
 
 
@@ -147,7 +223,7 @@ def session_ended_request_handler(handler_input):
 def all_exception_handler(handler_input, exception):
     # type: (HandlerInput, Exception) -> Response
     # Log the exception in CloudWatch Logs
-    print(exception)
+    print("Encountered the following exception: {}".format(exception))
 
     speech = "Sorry, I didn't get it. Can you please say it again!!"
     handler_input.response_builder.speak(speech).ask(speech)
--- src/weather.py
+++ src/weather.py
@@ -2,72 +2,141 @@ import requests
 import os
 from dotenv import load_dotenv
 import logging
+import tzlocal
+from uszipcode import SearchEngine
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 load_dotenv()
 
 
+def get_daily_values(item):
+    return datetime.fromtimestamp(float(item['time']), tzlocal.get_localzone())
+
+
 class DarkSkyApi(object):
-    def __init__(self, secret_key, coordinate_dict):
+    def __init__(self, zipcode, request_type):
         """ initialize coordinates and request """
-        self._secret_key = secret_key
-        self._coords = coordinate_dict
+        self.zipcode = zipcode
+        self.request_type = request_type
 
-        print(self._coords)
+        # print(self._coords)
 
-        self.LATITUDE = self._coords.get("latitude")
-        self.LONGITUDE = self._coords.get("longitude")
+
+        self.search = SearchEngine(simple_zipcode=True)
+        self.addr = self.search.by_zipcode(self.zipcode)
+        # self.LATITUDE = self._coords.get("latitude")
+        # self.LONGITUDE = self._coords.get("longitude")
+        self.LATITUDE = self.addr.lat
+        self.LONGITUDE = self.addr.lng
+
+
 
         # default to LR
         if self.LATITUDE is None or self.LONGITUDE is None:
             self.LATITUDE = os.getenv("LITTLE_ROCK_LAT")
             self.LONGITUDE = os.getenv("LITTLE_ROCK_LONG")
 
-        self.weatherRequest = "https://api.darksky.net/forecast/{}/{},{}?exclude=flags, " \
-                              "minutely".format(self._secret_key, self.LATITUDE, self.LONGITUDE)
+        self.weatherRequest = "https://api.darksky.net/forecast/75a9e562db1e04cbb51a8af5ee22b6b5/{},{}?exclude=flags, " \
+                              "minutely".format(self.LATITUDE, self.LONGITUDE)
 
-        print(self.weatherRequest)
 
     def get_weather(self):
         """ get weather from api and run it through parsing functions """
 
         print("Getting Weather JSON From API")
         weather_json = requests.get(self.weatherRequest).json()
-        parsed_json = None
 
+        parsed_json = None
         try:
             weather_json["error"]
         except KeyError:
             parsed_json = self.parse_weather_json(weather_json)
 
-        return parsed_json
+        if parsed_json and self.request_type == "current":
+            speech_text = self.build_current_weather_reply(parsed_json)
+        elif parsed_json and self.request_type == "hourly":
+            speech_text = self.build_daily_weather_reply(parsed_json)
+        elif parsed_json and self.request_type == "daily":
+            speech_text = self.build_hourly_weather_reply(parsed_json)
+        else:
+            speech_text = "Sorry, incorrect location for retrieving weather."
+
+        return speech_text
 
     def parse_weather_json(self, weather_json):
         """ parse weather json from api """
 
-        print(weather_json)
         currentWeatherSummary = weather_json['currently']['summary']
         hourlyWeatherSummary = weather_json['hourly']['summary']
         dailyWeatherSummary = weather_json['daily']['summary']
+        ct = self.convert_ts(weather_json['currently']['time'], tzlocal.get_localzone())
+        # ht = self.convert_ts(weather_json['hourly']['time'], tzlocal.get_localzone())
+        # dt = self.convert_ts(weather_json['daily']['time'], tzlocal.get_localzone())
 
-        # OPTIONAL ALERTS
-        # try:
-        #     alerts = self.weatherJson['alerts']
-        # except KeyError as e:
-        #     alerts = None
 
+
+        hourly_list = weather_json['hourly']['data']
+        hourly_times = list(map(lambda d: d['time'], hourly_list))
+        i = 0
+        while i < len(hourly_times):
+            hourly_times[i] = self.convert_ts(hourly_times[i], tzlocal.get_localzone())
+            i += 1
+
+        daily_list = weather_json['daily']['data']
+        daily_times = list(map(lambda d: d['time'], daily_list))
+        i = 0
+        while i < len(daily_times):
+            daily_times[i] = self.convert_ts(daily_times[i], tzlocal.get_localzone())
+            i += 1
+
+        print(hourly_times)
+        print(daily_times)
+        print(ct)
 
         parsed_weather_json = {
             "current_weather": currentWeatherSummary,
             "hourly_weather": hourlyWeatherSummary,
-            "daily_weather": dailyWeatherSummary
+            "daily_weather": dailyWeatherSummary,
+            "current_time": ct
         }
 
-        print(parsed_weather_json)
+        try:
+            alerts = weather_json['alerts']
+        except KeyError as e:
+            print(e)
+            alerts = None
 
+        res = {}
+        if alerts:
+            for line in alerts:
+                res.update(line)
+
+        for key in res:
+            parsed_weather_json.update({ "alert_" + key: res['{}'.format(key)] })
+
+        print(parsed_weather_json)
         return parsed_weather_json
 
+    def build_current_weather_reply(self, parsed_json):
+        speech_text = "The current forecast is "
+        speech_text += parsed_json["current_weather"] + "."
+        speech_text += " The current time is " + parsed_json['current_time'] + "."
+        return speech_text
+
+    def build_hourly_weather_reply(self, parsed_json):
+        speech_text = "The hourly forecast is "
+        speech_text += parsed_json["hourly_weather"] + "."
+        return speech_text
+
+    def build_daily_weather_reply(self, parsed_json):
+        speech_text = "The daily forecast is "
+        speech_text += parsed_json["daily_weather"]
+        return speech_text
+
+    def convert_ts(self, ts, tz):
+        return datetime.fromtimestamp(int(ts), tz).strftime('%Y-%m-%d %I:%M %p')
 
 def main():
     weather_coords = {
@@ -75,8 +144,9 @@ def main():
         "longitude": os.getenv("LITTLE_ROCK_LONG"),
     }
 
-    weatherObj = DarkSkyApi("75a9e562db1e04cbb51a8af5ee22b6b5", weather_coords)
+    weatherObj = DarkSkyApi("72023", "current")
     weather_dict = weatherObj.get_weather()
+    print(weather_dict)
 
 
 if __name__ == "__main__":
